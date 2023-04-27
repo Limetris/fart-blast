358e8652410b7453b0f9e1042ad626ae28c5a57a
dmitriy-lavrentev <dmitriy.lavrentev@wn.games>
2023-04-27 13:56:05 +0300

    - wave blast

--- assets/src/logic/tiles/bonus/TileBomb.ts
+++ assets/src/logic/tiles/bonus/TileBomb.ts
@@ -14,12 +14,27 @@ export class TileBomb extends  TileBonus {
     hit(): TilesHit {
         let tiles = super.hit();
 
-        for (let x = this.x - this.radius; x <= this.x + this.radius; x++) {
-            for (let y = this.y - this.radius; y <= this.y + this.radius; y++) {
-                if (this.x === x && this.y === y)
-                    continue;
-                Tile.tilesHitMerge(this.gameField.hitCell(x, y), tiles);
-            }
+        for (let r = 1; r <= this.radius; r++) {
+            let tilesHit = this._hitRadius(r);
+            tiles.push(...tilesHit);
+        }
+        return tiles;
+    }
+
+    private _hitRadius(radius: number = 1): TilesHit {
+        let tiles: TilesHit = [];
+
+        const xStart    = this.x - radius;
+        const xEnd      = this.x + radius;
+        const yStart    = this.y - radius;
+        const yEnd      = this.y + radius;
+        for (let x = xStart; x <= xEnd; x++) {
+            Tile.tilesHitMerge(this.gameField.hitCell(x, yStart), tiles);
+            Tile.tilesHitMerge(this.gameField.hitCell(x, yEnd), tiles);
+        }
+        for (let y = yStart + 1; y <= yEnd - 1; y++) {
+            Tile.tilesHitMerge(this.gameField.hitCell(xStart, y), tiles);
+            Tile.tilesHitMerge(this.gameField.hitCell(xEnd, y), tiles);
         }
         return tiles;
     }
